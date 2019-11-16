28d21d4dfdbe12e68e6590931d0fa5c2ef1c3de0
4ip-maker <57170626+4ip-maker@users.noreply.github.com>
2019-11-16 17:39:13 +0800

    selesai Main Activity 16/11/19 17.38

--- app/src/main/java/co/id/plnntb/kpkrecyclerview/Database.kt
+++ app/src/main/java/co/id/plnntb/kpkrecyclerview/Database.kt
@@ -1,6 +1,34 @@
 package co.id.plnntb.kpkrecyclerview
 
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import org.jetbrains.anko.db.ManagedSQLiteOpenHelper
+import org.jetbrains.anko.db.PRIMARY_KEY
+import org.jetbrains.anko.db.TEXT
+import org.jetbrains.anko.db.createTable
 
-class Database(ctx: Context): {
+class Database(ctx: Context): ManagedSQLiteOpenHelper(ctx, name = "kpk", factory = null, version = 1){
+    //buat companian object agar object database bisa diakses oleh semua activity
+    companion object {
+        private var instance: Database? = null
+        //syncronize object database untuk memastikan semua activity menggunakan object databaseuang sama
+        @Synchronized
+        fun getInstance(ctx: Context): Database{
+            if (instance==null){
+                instance = Database(ctx.applicationContext)
+            }
+            return instance!!
+        }
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db?.createTable("Pelanggan", true, "idpel" to TEXT + PRIMARY_KEY, "nama" to TEXT, "alamat" to TEXT, "tarif" to TEXT, "daya" to TEXT)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int){
+    }
 }
+
+//akses property dari Context
+val Context.database:Database
+    get() = Database.getInstance(applicationContext)
--- app/src/main/java/co/id/plnntb/kpkrecyclerview/MainActivity.kt
+++ app/src/main/java/co/id/plnntb/kpkrecyclerview/MainActivity.kt
@@ -2,34 +2,77 @@ package co.id.plnntb.kpkrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings.Global.getString
+import android.provider.SyncStateContract.Helpers.insert
 import androidx.recyclerview.widget.LinearLayoutManager
 import co.id.plnntb.kpkrecyclerview.adapter.PelangganAdapter
 import co.id.plnntb.kpkrecyclerview.model.Pelanggan
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.db.createTable
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.db.select
+import org.jetbrains.anko.selector
 
 
 class MainActivity : AppCompatActivity() {
 
     //buat variable untuk menampung data pelanggan
     var listPlg = mutableListOf<Pelanggan>()
+    val adapterPlg = PelangganAdapter(listPlg)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //tambahkan data kedalam variable listPLg
-        listPlg.add(Pelanggan("441234567890","Pelanggan A", "mataram", "B2","197000"))
-        listPlg.add(Pelanggan("441234567890","Pelanggan B", "mataram", "B2","41500"))
-        listPlg.add(Pelanggan("441234567890","Pelanggan C", "mataram", "B2","555000"))
-        listPlg.add(Pelanggan("441234567890","Pelanggan D", "mataram", "B2","82500"))
-        listPlg.add(Pelanggan("441234567890","Pelanggan E", "mataram", "B2","197000"))
-        listPlg.add(Pelanggan("441234567890","Pelanggan F", "mataram", "B2","66000"))
+        initTampilan()
 
         //masukkan data pelanggan ke Recycler View
         rv.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = PelangganAdapter(listPlg)
+            adapter = adapterPlg
         }
 
     }
+    fun initTampilan(){
+       btnsimpan.setOnClickListener {
+           database.use {
+               insert("pelanggan",
+                   "idpel" to idpel.text,
+                   "nama" to nama.text.toString(),
+                   "alamat" to alamat.text.toString(),
+                   "tarif" to tarif.text.toString(),
+                   "daya" to daya.text.toString()
+                   )
+               listPlg.add(Pelanggan(
+                   idpel.text.toString(),
+                   nama.text.toString(),
+                   alamat.text.toString(),
+                   tarif.text.toString(),
+                   daya.text.toString()
+                   )
+               )
+           }
+       }
+    }
+
+    override fun onResume() {
+        bacaDatabase()
+        super.onResume()
+    }
+
+    fun bacaDatabase(){
+        database.use {
+            select("pelanggan").exec {
+                listPlg.clear()
+                while (this.moveToNext()){
+                    listPlg.add(Pelanggan(
+                        getString(getColumnIndex("idpel")),
+                        getString(getColumnIndex("nama")),
+                        getString(getColumnIndex("alamat")),
+                        getString(getColumnIndex("tarif")),
+                        getString(getColumnIndex("daya"))
+                    ))
+                }
+
+            }
+        }}
 }
